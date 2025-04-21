50a267a1c7cb79f6800c9d31fb3d8bfa16d08686
hoangpdh <huyhoang.190904@gmail.com>
2025-04-21 12:47:42 +0700

    Initial commit

--- utils.py
+++ utils.py
@@ -1,44 +1,19 @@
+# utils.py
 import torch
 
 def collate_fn(batch):
-    """
-    Collate function to batch images and reports.
-    Args:
-        batch (list of tuples): Each element is (image_tensor, report_str)
-    Returns:
-        images (Tensor): Batched images of shape (B, C, H, W)
-        reports (list of str): List of report strings
-    """
     images, reports = zip(*batch)
     images = torch.stack(images, dim=0)
     return images, list(reports)
 
-
 def compute_bleu(preds, refs):
-    """
-    Compute BLEU score using sacrebleu.
-    Args:
-        preds (list of str): Generated texts
-        refs (list of str): Reference texts
-    Returns:
-        float: BLEU score
-    """
     try:
         import sacrebleu
         return sacrebleu.corpus_bleu(preds, [refs]).score
     except ImportError:
         raise ImportError("Please install sacrebleu to compute BLEU scores.")
 
-
 def compute_rouge(preds, refs):
-    """
-    Compute average ROUGE-1 and ROUGE-L F1 scores.
-    Args:
-        preds (list of str)
-        refs (list of str)
-    Returns:
-        dict: {'rouge1': float, 'rougeL': float}
-    """
     try:
         from rouge_score import rouge_scorer
         scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
